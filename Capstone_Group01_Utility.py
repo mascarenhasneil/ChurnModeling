06b11223005606704b8f1882eb3069ae2842f8d4
Neil Mascarenhas <neilpm1995@gmail.com>
2021-07-07 16:26:51 -0400

    Import Libraries and Created Utility

---
+++ Capstone_Group01_Utility.py
@@ -0,0 +1,17 @@
+"""
+Final Capstone Project. Group 1
+ALY6140 80956 Analytics Systems Technology SEC 04 Spring 2021 CPS
+
+Title: Customer Churn Modeling.
+
+Copyright (c) 2021
+Licensed
+Written by Neil Mascarenhas
+Date: 10 May 2021
+"""
+
+
+
+def Test():   # Not required but a good practice to check if this file is accessible 
+    print("Capstone Test Successful")
+
